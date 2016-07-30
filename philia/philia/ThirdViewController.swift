3c853b8f15e30ed89a40ec42d89c377501069052
khimung <kim2kim@gmail.com>
2016-07-30 00:16:53 -0700

    Add thirdViewcontroller - did not get checked in

---
+++ philia/philia/ThirdViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ThirdViewController.swift
+//  philia
+//
+//  Created by khim ung on 7/29/16.
+//  Copyright © 2016 khim ung. All rights reserved.
+//
+
+import UIKit
+
+/**
+ *
+ */
+class ThirdViewController: UIViewController {
+    
+    @IBOutlet var womenButton: UIButton!
+    @IBOutlet var menButton: UIButton!
+    @IBOutlet var bothButton: UIButton!
+    
+    @IBOutlet var debugLabel: UILabel!
+    
+    var name:String = ""
+    
+    var userInterest: Int = 0
+    
+    var userGenderPreference: Int = 0
+    
+    @IBAction func pressedButton(sender: UIButton) {
+        let answer: Int = Int(sender.tag)
+        
+        userGenderPreference = answer | userGenderPreference
+        
+        debugLabel.text = "\(userGenderPreference) and \(userInterest) for \(name)"
+        
+        
+        
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        let fourthViewController = storyBoard.instantiateViewControllerWithIdentifier("fourthViewController") as! FourthViewController
+        
+        fourthViewController.userInterest = userInterest
+        fourthViewController.name = name
+        fourthViewController.userGenderPreference = userGenderPreference
+        
+        // ---- OR ----
+        
+        // If you want to present the new ViewController then use this
+        self.presentViewController(fourthViewController, animated:true, completion:nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.womenButton.layer.cornerRadius = 10
+        self.menButton.layer.cornerRadius = 10
+        self.bothButton.layer.cornerRadius = 10
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
